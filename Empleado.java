11d5626fee042934a41325df5e5be4014280070c
Luis-Chiliquinga <luischiliquinga54@gmail.com>
2024-12-07 19:39:21 -0500

    Create Empleado.java

---
+++ Empleado.java
@@ -0,0 +1,13 @@
+public class Empleado extends Persona {
+    public Empleado(String nombre, int ID, String nivelDeAcceso, String horaEntrada, String horaSalida) {
+        super(nombre, ID, nivelDeAcceso, horaEntrada, horaSalida);
+    }
+
+    public void atenderClientes() {
+        System.out.println(getNombre() + " está atendiendo a un cliente.");
+    }
+
+    public void resolverProblemas() {
+        System.out.println(getNombre() + " está resolviendo problemas.");
+    }
+}
