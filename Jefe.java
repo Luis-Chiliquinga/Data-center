5b07b14ea2a7678d3e2fa52df34aeea16a55a015
Luis-Chiliquinga <luischiliquinga54@gmail.com>
2024-12-07 19:41:41 -0500

    Create Jefe.java

---
+++ Jefe.java
@@ -0,0 +1,13 @@
+public class Jefe extends Persona {
+    public Jefe(String nombre, int ID, String nivelDeAcceso, String horaEntrada, String horaSalida) {
+        super(nombre, ID, nivelDeAcceso, horaEntrada, horaSalida);
+    }
+
+    public void nivelDeAutorizacion() {
+        System.out.println(getNombre() + " tiene un alto nivel de autorización.");
+    }
+
+    public void registrarDatosEmpleados() {
+        System.out.println(getNombre() + " está registrando los datos de los empleados.");
+    }
+}
