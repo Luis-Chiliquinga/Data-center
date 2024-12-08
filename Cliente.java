6c508f41470a3ac7661e27491343e0643ba8ece3
Luis-Chiliquinga <luischiliquinga54@gmail.com>
2024-12-07 19:58:34 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -7,7 +7,7 @@ public class Cliente extends Persona {
         System.out.println(getNombre() + " es un cliente frecuente.");
     }
 
-    public void reportarProblema() {
+    private void reportarProblema() {
         System.out.println(getNombre() + " ha reportado un problema.");
     }
 }
